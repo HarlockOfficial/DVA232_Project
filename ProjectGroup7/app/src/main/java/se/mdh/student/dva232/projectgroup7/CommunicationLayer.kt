4b7e91e4386c636213aea220fb8433f534fdac70
HarlockOfficial <harlockofficial.github@gmail.com>
2020-12-12 16:17:52 +0100

    app<->server connection completed not tested

---
+++ ProjectGroup7/app/src/main/java/se/mdh/student/dva232/projectgroup7/CommunicationLayer.kt
@@ -0,0 +1,40 @@
+package se.mdh.student.dva232.projectgroup7
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import java.net.URL
+import java.util.UUID
+
+object CommunicationLayer {
+    private val url: String = "https://dva232-project-group-7.000webhostapp.com/?player="+UUID.randomUUID().toString()
+
+    suspend fun addPlayerToMultiplayerQueue(data: Data) {
+        withContext(Dispatchers.IO) {
+            return@withContext JSONObject(
+                    URL("$url&action=add_queue&game=${data.game.name}").readText()
+            )
+        }
+    }
+    suspend fun checkMultiplayerQueue(data: Data) {
+        withContext(Dispatchers.IO) {
+            return@withContext JSONObject(
+                    URL("$url&action=get_queue&game=${data.game.name}").readText()
+            )
+        }
+    }
+    suspend fun addPlayerMove(data: Data) {
+        withContext(Dispatchers.IO) {
+            return@withContext JSONObject(
+                    URL("$url&action=add_move&game=${data.game.name}&move=${data.moveToCsv()}").readText()
+            )
+        }
+    }
+    suspend fun getOpponentMove(data: Data) {
+        withContext(Dispatchers.IO) {
+            return@withContext JSONObject(
+                    URL("$url&action=get_move&game=${data.game.name}").readText()
+            )
+        }
+    }
+}
---
+++ ProjectGroup7/app/src/main/java/se/mdh/student/dva232/projectgroup7/GameType.kt
@@ -0,0 +1,7 @@
+package se.mdh.student.dva232.projectgroup7
+
+enum class GameType(private val code: String) {
+    TIC_TAC_TOE("ttt"),
+    ROCK_PAPER_SCISSORS("rps"),
+    DICES("dices")
+}
