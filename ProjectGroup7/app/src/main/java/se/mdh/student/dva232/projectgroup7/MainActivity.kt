d7253ecea00f34e1224604ffb4156d6ebf1b967f
HarlockOfficial <harlockofficial.github@gmail.com>
2020-12-12 19:53:03 +0100

    gradle upgrade

--- ProjectGroup7/app/src/main/java/se/mdh/student/dva232/projectgroup7/MainActivity.kt
+++ ProjectGroup7/app/src/main/java/se/mdh/student/dva232/projectgroup7/MainActivity.kt
@@ -3,6 +3,7 @@ package se.mdh.student.dva232.projectgroup7
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// https://github.com/codepath/android_guides/wiki/Fragment-Navigation-Drawer
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
