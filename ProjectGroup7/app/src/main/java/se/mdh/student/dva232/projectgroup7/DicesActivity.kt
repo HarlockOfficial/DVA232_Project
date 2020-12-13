c8b45519ae4c19863dc9d7845666fb96c852afca
GrillexN <alfons.jansson98@gmail.com>
2020-12-13 18:09:14 +0100

    Very basic functionality of dices game. Missing data fetch from API and multiplayer functionality. No interface

--- ProjectGroup7/app/src/main/java/se/mdh/student/dva232/projectgroup7/DicesActivity.kt
+++ ProjectGroup7/app/src/main/java/se/mdh/student/dva232/projectgroup7/DicesActivity.kt
@@ -2,35 +2,51 @@ package se.mdh.student.dva232.projectgroup7
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class DicesActivity : AppCompatActivity() {
 
 
+    private var dicesMap: HashMap<String, Int> = HashMap()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dices2)
+        val button = findViewById<Button>(R.id.button_rtd)
+        button.setOnClickListener{
+            setDicesMap(4)
+            displayResult()
+        }
 
-        roll(4)
+
+
+    }
+
+    //sets dice map and applies results from
+    private fun setDicesMap(noOfDice: Int) {
+        for (e in 0 until noOfDice) {
+            //val number = e + 1                          Not worth it..?
+            dicesMap["Dice $e"] = e                     //Replace e with a function that gets a number from result
+        }
+    }
+
+
+    //Displays the sum of all dices.
+    private fun displayResult() {
+
+        val text = findViewById<TextView>(R.id.resulttext)
+        var sum = 0
+
+        for ((key: String, value: Int) in dicesMap) {
+            sum += value
+        }
+
+        text.text = sum.toString()
     }
 
     //Get data from JSON file.
-   private fun getData(array: Array<Int>, noOfDice: Int): Array<Int> {
-
-        //Temporary fill array functionality. Replace!
-        for (e in 0..noOfDice) {
-            array[e] = e+10
-        }
-        return array
-    }
-
-    //Call on button. Calls getData. Array is created in function
-    fun roll (noOfDice: Int) {
-        var diceArray = arrayOf(Int)
-        diceArray = getData(diceArray,noOfDice) //Fills array
-
-        for (e in diceArray) {
-            println(diceArray[e])
-        }
-
+    private fun getData(): Int {
+        TODO("get information from API, then return the int.")
     }
 }
