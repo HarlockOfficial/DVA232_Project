9b06db9eb7e4d81e42f18c60187995b068f2695b
GrillexN <alfons.jansson98@gmail.com>
2020-12-16 14:56:01 +0100

    Shaking phone now rolls the dice

--- ProjectGroup7/app/src/main/java/se/mdh/student/dva232/projectgroup7/DicesActivity.kt
+++ ProjectGroup7/app/src/main/java/se/mdh/student/dva232/projectgroup7/DicesActivity.kt
@@ -1,18 +1,34 @@
 package se.mdh.student.dva232.projectgroup7
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 
-class DicesActivity : AppCompatActivity() {
+class DicesActivity : AppCompatActivity(), SensorEventListener {
 
 
     private var dicesMap: HashMap<String, Int> = HashMap()
+    private lateinit var sensorManager : SensorManager
+    private var accelerometer: Sensor? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dices2)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+
+
+        val accelerometer: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME)
+
+
         val button = findViewById<Button>(R.id.button_rtd)
         button.setOnClickListener{
             setDicesMap(4)
@@ -21,6 +37,29 @@ class DicesActivity : AppCompatActivity() {
 
 
 
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event != null) {
+            if (event.values[0] > 5 || event.values[1] > 10|| event.values[2] > 1)
+            setDicesMap(4)
+            displayResult()
+    }
+
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+    }
+
+    override fun onResume() { //No idea if this is working properly
+        super.onResume()
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
     }
 
     //sets dice map and applies results from
@@ -32,13 +71,13 @@ class DicesActivity : AppCompatActivity() {
     }
 
 
-    //Displays the sum of all dices.
+    //Displays the sum of all dices. This is the only visual change for the user. https://developer.android.com/guide/topics/sensors/sensors_motion for sensors. Sensor calibration? 
     private fun displayResult() {
 
         val text = findViewById<TextView>(R.id.resulttext)
         var sum = 0
 
-        for ((key: String, value: Int) in dicesMap) {
+        for ((key: String, value: Int) in dicesMap) { //Key behövs inte?
             sum += value
         }
 
