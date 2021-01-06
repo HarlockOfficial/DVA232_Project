30508f32697346acfbcd9f45294962ec3624e093
Zylan <zylanshukr@hotmail.com>
2021-01-06 21:13:45 +0100

    Settings, Music

--- ProjectGroup7/app/src/main/java/se/mdh/student/dva232/projectgroup7/MainActivity.kt
+++ ProjectGroup7/app/src/main/java/se/mdh/student/dva232/projectgroup7/MainActivity.kt
@@ -2,10 +2,91 @@ package se.mdh.student.dva232.projectgroup7
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.Bundle
+import android.os.IBinder
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceManager
+
 
 class MainActivity : AppCompatActivity() {
+
+    //---------------------------------------------------------------------------------------//
+    private var mService: MusicService? = null
+    private var mBound: Boolean = false
+
+    private val connection = object : ServiceConnection {
+
+        override fun onServiceConnected(className: ComponentName, service: IBinder) {
+            val binder = service as MusicService.ServiceBinder
+            mService = binder.getService()
+            mBound = true
+        }
+
+        override fun onServiceDisconnected(arg0: ComponentName) {
+            mBound = false
+        }
+    }
+    //---------------------------------------------------------------------------------------//
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    override fun onStop() {
+        super.onStop()
+        if(mBound) {
+            unbindService(connection)
+            mBound = false
+        }
+
+    }
+    //---------------------------------------------------------------------------------------//
+
+    override fun onResume() {
+        super.onResume()
+        if(isBackgroundEnabled(applicationContext)){
+            //startService(Intent(this, MusicService::class.java))
+            Intent(this, MusicService::class.java).also { intent ->
+                bindService(intent, connection, Context.BIND_AUTO_CREATE)
+                startService(intent)
+                //mService?.resumeMusic()
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mService?.pauseMusic()
+        //stopService(Intent(this, MusicService::class.java))
+    }
+
+    fun isBackgroundEnabled(context: Context): Boolean {
+        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+        return prefs.getBoolean(("switch_preference_music"), false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menuitem, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.settings -> {
+                this.startActivity(Intent(this, Settings::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
