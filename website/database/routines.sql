ba73e0feef1f1b457d461bac27864b88b592a5fc
HarlockOfficial <harlockofficial.github@gmail.com>
2021-01-11 18:43:42 +0100

    Update routines.sql

--- website/database/routines.sql
+++ website/database/routines.sql
@@ -36,7 +36,8 @@ CREATE FUNCTION `add_move` (`_playerCode` VARCHAR(20), `_gameCode` VARCHAR(10), 
 				delete from last_move where game_id=_game_id and player_code!=_playerCode; 
 				select convert(_field, signed) into _temporary;
 				select convert(_field_tmp, signed) into _dice_sum;
-				update current_matches set field=_temporary+(_position-_dice_sum), player_code_1=player2, player_code_2=_playerCode where game_code=_gameCode and (player_code_1=_playerCode or player_code_2=_playerCode);
+				set _temporary = _temporary+(_position-_dice_sum);
+				update current_matches set field=_temporary, player_code_1=player2, player_code_2=_playerCode where game_code=_gameCode and id=_game_id;
 				select field into _field from current_matches where game_code=_gameCode and (player_code_1=_playerCode or player_code_2=_playerCode);
 				return _field;
 			end if;
